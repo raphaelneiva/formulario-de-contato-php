3b6bb8a474c718204a39942e501f992c05689bd0
Raphael Neiva <raphaelpneiva@gmail.com>
2019-02-23 12:01:38 -0300

    Formulário criado

--- index.php
+++ index.php
@@ -4,11 +4,28 @@
         <title> Formulário para Contato </title>
     </head>
     <body>
-        <h1> Formulário  </h1>
+        <h1> Formulário de Contato </h1>
         <?php
 
 
         ?>
+        <form method = "post" action = "processar_cadastra_mensagem.php"> 
+            <label> Nome: </label>
+            <input type = "text" name = "nome" placeholder = "Nome Completo "> <br> <br>
+
+            <label> E-mail: </label>
+            <input type = "email" name = "email" placeholder = "E-mail"> <br> <br>  
+
+            <label> Assunto: </label>
+            <input type = "text" name = "assunto" placeholder = "Assunto"> <br> <br>
+
+            <label> Mensagem </label>
+            <textarea name = "mensagem" rows  = "3" cols = "40"> </textarea> <br> <br>
+
+            <input name = "enviar" type = "submit" value = "Enviar" >
+
+        </form>
+
     </body>
 
 </html>       
